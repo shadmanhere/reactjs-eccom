f8efbbf93b256349cb5efa84bbb53493d4217c5c
Shadman Ali <shadman.ali@live.co.uk>
2022-05-07 17:37:43 +0530

    navbar added

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,38 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <NavWrapper className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <Link class="navbar-brand ms-3" to="#">
+            <img src="/logo192.png" alt="Avatar Logo" style={{width:"40px"}} className="rounded-pill" /> 
+        </Link>
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+        </ul>
+    </NavWrapper>  
   )
 }
+
+const NavWrapper = styled.nav`
+    .nav-pills {
+      background:white;
+      border-radius:5px;
+      &:hover{
+          background:gray;
+          color:white;
+      }
+    }
+    .nav-link {
+      color:black;
+      &:hover{
+          color:white;
+      }
+    }
+`
 
 export default Header
