5519f9a689284ecd2238fac7b9a5bf10ec3446b0
Shadman Ali <shadman.ali@live.co.uk>
2022-05-07 15:50:40 +0530

    switched to redux tool kit as createStore is deprecated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,5 @@
-import{ createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 
-function basicStore(state = [], action) {
-    switch (action.type) {
-        case 'STORE_BASIC':
-            return state.concat([action.text]);
-        default:
-            return state;
-    }
-}
-const store = createStore(basicStore, ['Use Redux']);
-
-store.dispatch({
-    type: 'STORE_BASIC',
-    text: 'Basic store setup'
+export default configureStore({
+  reducer: {}
 })
-
-console.log(store.getState());
-
-export default store;
-
